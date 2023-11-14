2b41acc6ddc3b9864caa8efe37d0cf0c9d58f736
ICFarrar <farraricollege4@tamu.edu>
2023-11-13 19:27:57 -0600

    Update live plotting 8 channel.py
    Add color, sweeping plotting mode, and plotting real data from CSV

--- live plotting 8 channel.py
+++ live plotting 8 channel.py
@@ -2,12 +2,23 @@ import pyqtgraph as pg
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QTimer
 import numpy as np
+import csv
+import pandas as pd
+
+#parameters customizing plot
+x_axis_size = 250 # sets how many data points are kept for plotting
+time_between_updates = 1 # sets how often the plots update function is called, in ms
+window = 1
+
+#load data from csv to numpy array
+df = pd.read_csv("MyoDataLive.csv", dtype="Int64", delimiter = ";")
+print(df)
+csv_data = df.to_numpy()
+
 
 #vectors holding data being plotted
-x_axis_size = 100 # sets how many data points are kept for plotting
-time_between_updates = 100 # sets how often the plots update function is called, in ms
-
 sensor_vectors = np.zeros((8,x_axis_size,)) # different sensor_vectors for each sensor
+
 
 # Create a PyQtGraph application
 app = QApplication([])
@@ -20,25 +31,49 @@ win.setWindowTitle('Live Vector Plot')
 plots = []
 
 # Initialize the plots with the initial data
+colorList = ["#fa9189", "#fcae7c",
+                      "#ffe699", "#f9ffb5", "#b3f5bc", "#d6f6ff", "#e2cbf7", "#d1bdf1"]
 plot = '1'
 plot_position_iterator = 0 # used to determine when to create new row or column
 for vector in sensor_vectors:
-    plot_position_iterator += 1
     plot = win.addPlot(title=f"Vector Plot {plot}")
-    curve = plot.plot(vector, pen='g')
+    plot.setXRange(0,x_axis_size)
+    curve = plot.plot(vector, pen=colorList[plot_position_iterator])
     plots.append({'plot': plot, 'curve': curve})
-    if plot_position_iterator % 2 == 0:
+    if plot_position_iterator % 2 == 1:
         win.nextRow()
-    elif plot_position_iterator % 2 == 1:
+    elif plot_position_iterator % 2 == 0:
         win.nextCol()
+    plot_position_iterator += 1
 
 # Function to update the plots with new data
+x_value = 0
+data_index = 0
 def update_plots():
-    fake_data = np.random.rand(1, 8) #creates fake data in place of sensor data
-    for i, vector in enumerate(sensor_vectors):
-        sensor_vectors[i] = np.roll(sensor_vectors[i], -1)  # shift data to the left
-        sensor_vectors[i][-1] = fake_data[0][i]  # add new data point to end
-        plots[i]['curve'].setData(vector) # update plot
+    global x_value
+    global data_index
+
+    if 0: #0 for normal shift left plotting, 1 for sweeping with wrap around
+        for i in range(8):
+            # sensor_vectors[i][x_value] = fake_data[0][i]
+            sensor_vectors[i][x_value] = csv_data[data_index][i]
+            plots[i]['curve'].setData(sensor_vectors[i])
+            if x_value == x_axis_size - 1:
+                sensor_vectors[i][0] = 0
+            else:
+                sensor_vectors[i][x_value+1] = 0
+            plots[i]['curve'].setData(sensor_vectors[i])
+        x_value += 1
+        if x_value == x_axis_size:
+            x_value = 0
+    else:
+        for i, vector in enumerate(sensor_vectors):
+            sensor_vectors[i] = np.roll(sensor_vectors[i], -1)  # shift data to the left
+            sensor_vectors[i][-1] = csv_data[data_index][i]  # add new data point to end
+            plots[i]['curve'].setData(vector) # update plot
+    data_index += 1
+    if(data_index == csv_data.size): #return to start of csv file
+        data_index = 0
 
 # Create a QTimer to periodically update the plots (e.g., every 100 ms)
 timer = QTimer()
